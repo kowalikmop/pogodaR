317583f21335a460366fbc5d50de7e24c4146f9d
Marcin Kowalik <kowalikm@op.pl>
2023-11-01 10:47:28 +0100

    Add BEM methodology to project

--- src/components/Contener.js
+++ src/components/Contener.js
@@ -9,42 +9,47 @@ const Contener = () => {
   return (
     <div className="contener">
       <div className="title">
-        <h1>Średnia temperatura w {month[0]}</h1>
-        <p>Rozwiń</p>
+        <div className="title__text">
+          Średnia temperatura w {month[0]}
+        </div>
+        <div className="title__button">Rozwiń</div>
       </div>
       <div className="article">
         <div className="section">
-          <div className="picture">
+          <div className="section__picture">
             {" "}
             <img src={Sun} alt="Sun" />
           </div>
-          <p>17</p>
-          <div className="degrees"> {" "}
-            <img src={Degrees} alt="Degrees" /></div>
-          <div className="degrees_title">Temperatura dzień/noc</div>
-        </div>
-        <div className="section">
-        <div className="picture">
-            {" "}
-            <img src={Beach} alt="Beach" />
-          </div>
-          <p>17</p>
-          <div className="degrees">            
-            {" "}
-            <img src={Degrees} alt="Degrees" /></div>
-          <div className="degrees_title">Temperatura dzień/noc</div>
-        </div>
-        <div className="section">
-        <div className="picture">
-            {" "}
-            <img src={Sunhappy} alt="Sunhappy" />
-          </div>
-          <p>17</p>
-          <div className="degrees">
+          <div className="section__text">17</div>
+          <div className="section__picture--degrees">
             {" "}
             <img src={Degrees} alt="Degrees" />
           </div>
-          <div className="degrees_title">Temperatura dzień/noc</div>
+          <div className="section__text--title">Temperatura dzień/noc</div>
+        </div>
+        <div className="section">
+          <div className="section__picture">
+            {" "}
+            <img src={Beach} alt="Beach" />
+          </div>
+          <div className="section__text">17</div>
+          <div className="section__picture--degree">
+            {" "}
+            <img src={Degrees} alt="Degrees" />
+          </div>
+          <div className="section__text--title">Temperatura dzień/noc</div>
+        </div>
+        <div className="section">
+          <div className="section__picture">
+            {" "}
+            <img src={Sunhappy} alt="Sunhappy" />
+          </div>
+          <div className="section__text">17</div>
+          <div className="section__picture--degree">
+            {" "}
+            <img src={Degrees} alt="Degrees" />
+          </div>
+          <div className="section__text--title">Temperatura dzień/noc</div>
         </div>
       </div>
     </div>
